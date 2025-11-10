049218dcff68710c14ca7c0490a9a6fc198dd51d
rohitkast <kasturirohitrk@gmail.com>
2025-11-10 04:38:25 +0000

    black window opened

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+from logger import log_state
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+def main():
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill('black')
+        pygame.display.flip()
+    # version = pygame.version.ver
+    # print(f"Starting Asteroids! with pygame version: {version}")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
